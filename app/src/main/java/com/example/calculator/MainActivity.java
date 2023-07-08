fa04d226fcd687e87bf95a8b2750aeb36a62d583
sarfraz-01010 <sarfraz01010@gmail.com>
2023-07-08 22:44:10 +0500

    objects are made

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -17,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.editTextTextPersonName);
+        num2 = findViewById(R.id.editTextTextPersonName2);
+        sum = findViewById(R.id.button);
+        subtract = findViewById(R.id.button2);
+        multiply = findViewById(R.id.button3);
+        divide = findViewById(R.id.button4);
+
+        text = findViewById(R.id.textView2);
     }
 }
